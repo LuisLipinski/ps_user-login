a9a5a34e36726c12b3e94ef4196519ba0e71d46c
LuisLipinski <98597288+LuisLipinski@users.noreply.github.com>
2026-08-27 10:16:17 -0300

    fix: corrige import de request binding do Spring

--- src/main/java/com/mypetadmin/ps_user/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mypetadmin/ps_user/exception/GlobalExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.mypetadmin.ps_user.exception;
 
-import jakarta.servlet.ServletRequestBindingException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
